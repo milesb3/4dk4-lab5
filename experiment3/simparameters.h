de4e1d1a547368c4f8b55f005584917d86254c0b
Ben Miles <milesb3@mcmaster.ca>
2024-11-26 13:03:17 -0500

    Adds experiment 3a simulation results

--- experiment3/simparameters.h
+++ experiment3/simparameters.h
@@ -5,14 +5,14 @@
 
 #define PACKET_ARRIVAL_RATE 1   /* mean arrival rate (packets/s) of data packets */
 #define T 1 /* token generation period (a token is generated every T seconds)*/
-#define B_T_MAX 1
+#define B_T_MAX 10
 /* token bucket maximum capacity */
-#define B_D_MAX 1
+#define B_D_MAX 10
 /* data bucket maximum capacity */
 #define RUNLENGTH 70000000
 #define BLIPRATE 100000
 
-#define OUTPUT_CSV_FILE "results/experiment3a-1-1.csv"
+#define OUTPUT_CSV_FILE "results/experiment3a-10-10.csv"
 #define CSV_COLUMN_HEADER "random seed, packet arrival rate, T, B_T_MAX, B_D_MAX, packet arrival count, number of packets lost, number of packets transmitted, packet loss rate, average output data rate\n"
 
 /* Comma separated list of random seeds to run. */
